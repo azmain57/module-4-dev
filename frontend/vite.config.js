1963f85438b6a77789117d72108cd050a016c8b6
azmain57 <azmain5718@gmail.com>
2025-12-24 04:42:37 +0600

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://svc-app-backtest-93fc6314:3000',
         changeOrigin: true
       }
     }
